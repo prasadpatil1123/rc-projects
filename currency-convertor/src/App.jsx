90c0128d9a8ed0954986a549171277a1a321e265
prasadpatil1123 <122286888+prasadpatil1123@users.noreply.github.com>
2024-04-02 20:51:59 +0530

    App base

---
+++ currency-convertor/src/App.jsx
@@ -0,0 +1,11 @@
+export default function App() {
+  return (
+   <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
+    {/* <div className="container"> */}
+    <h1 className=" font-bold  ">
+  Hello Kashi!
+    </h1>
+    {/* </div> */}
+   </div>
+  )
+}
---
+++ currency-convertor/src/components/currencyconvertor.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const currencyconvertor = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default currencyconvertor;
